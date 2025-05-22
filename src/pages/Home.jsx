d02a3e4746ec25526fd933bbe847b9fc2c906844
natsilp48 <natsilprasert@gmail.com>
2025-05-22 19:11:29 +0700

    Second commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+
+function Home() {
+    return (
+        <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
+
+            {/* { Theme Toggle } */}
+
+            {/* { BG Effect } */}
+
+            {/* { Navbar } */}
+
+            {/* { Main Content } */}
+
+            { /* {Footer } */}
+
+        </div>
+    )
+}
+
+export default Home
